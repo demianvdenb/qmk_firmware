43ae8ba070c6e973122158493a4cbe0fd1949cde
Demian Van den Berghe <demian.vandenberghe@team.blue>
2025-05-11 15:29:51 +0200

    Adding layer toggle switch

--- keyboards/keychron/q10_pro/ansi_encoder/keymaps/home_row_mods/keymap.c
+++ keyboards/keychron/q10_pro/ansi_encoder/keymaps/home_row_mods/keymap.c
@@ -9,9 +9,21 @@
 #define HM_J  MT(MOD_LGUI, KC_J)
 #define HM_K  MT(MOD_RSFT, KC_K)
 #define HM_L  MT(MOD_RALT, KC_L)
-#define HM_SCL MT(MOD_LCTL, KC_SCLN)
+#define HM_SCLN MT(MOD_LCTL, KC_SCLN)
 
 #define LT_SPC LT(LAYER_2, KC_SPC)
+
+enum layers{
+    LAYER_1,
+    LAYER_2,
+    LAYER_3,
+    LAYER_4,
+};
+
+enum custom_keycodes {
+    MC_SCREENSHOT = SAFE_RANGE,
+    MC_LOCK,
+};
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -30,12 +42,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-enum custom_keycodes {
-    MC_SCREENSHOT = SAFE_RANGE,
-    MC_LOCK,
-};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static bool is_layer_1_active = false;
     switch (keycode) {
         case MC_SCREENSHOT:
             if (record->event.pressed) {
@@ -48,23 +56,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code16(G(KC_ESC));
             }
             break;
+        case MC_1:
+            if (record->event.pressed) {
+                if (is_layer_1_active) {
+                    layer_move(LAYER_3);  // switch to Layer 3
+                } else {
+                    layer_move(LAYER_1);  // switch to Layer 1
+                }
+                is_layer_1_active = !is_layer_1_active;
+            }
+            break;
     }
     return true;
 }
 
-enum layers{
-    LAYER_1,
-    LAYER_2,
-    LAYER_3,
-    LAYER_4,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_1] = LAYOUT_90_ansi(
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_F10,   KC_F11,   KC_F12,   MC_SCREENSHOT,      MC_LOCK,
-        MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   _______,            KC_PGUP,
+        MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MC_3,       KC_ESC,   HM_A,     HM_S,     HM_D,     HM_F,     KC_G,      KC_H,     HM_J,     HM_K,     HM_L,     HM_SCL,   KC_QUOT,            KC_ENT,             KC_HOME,
+        MC_3,       KC_ESC,   HM_A,     HM_S,     HM_D,     HM_F,     KC_G,      KC_H,     HM_J,     HM_K,     HM_L,     HM_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         MC_4,       _______,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  _______,  KC_UP,
         MC_5,       KC_LCTL,  KC_LWIN,  KC_NO,    KC_BSPC,  KC_DEL,                        LT_SPC,             _______,            _______,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -81,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
         MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MC_3,       KC_ESC,   HM_A,     HM_S,     HM_D,     HM_F,     KC_G,      KC_H,     HM_J,     HM_K,     HM_L,     HM_SCL,   KC_QUOT,            KC_ENT,             KC_HOME,
+        MC_3,       KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_BSPC,  KC_DEL,              LT_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   KC_DEL,              LT_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [LAYER_4] = LAYOUT_90_ansi(
         RGB_TOG,    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
